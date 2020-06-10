33633ec0e219ce4853046457e73a4d071e814cc4
Aditya Maulana <dait23@gmail.com>
2020-06-10 11:11:50 +0700

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +17,11 @@ function App() {
         <p>
           Travis Ci Demo
         </p>
+        <button 
+           className="center-block" 
+           onClick={handleClick}>
+         {count}
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
