72fe8865ac390608e2acf11344747ffc3650db87
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-14 10:44:50 +0000

    Deploy dait23/travis-demo to github.com/dait23/travis-demo.git:gh-pages

---
+++ precache-manifest.73fece7ce52a663af2f44e4b92804002.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8518040c342fd26834d2b764c78bea15",
+    "url": "/travis-demo/index.html"
+  },
+  {
+    "revision": "81aa65c2a11b247ac072",
+    "url": "/travis-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19b459ed146830828d84",
+    "url": "/travis-demo/static/js/2.a484475c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travis-demo/static/js/2.a484475c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81aa65c2a11b247ac072",
+    "url": "/travis-demo/static/js/main.1a115837.chunk.js"
+  },
+  {
+    "revision": "e9147758478e86f54303",
+    "url": "/travis-demo/static/js/runtime-main.7bc77ea8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
